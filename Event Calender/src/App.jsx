e1311bb51fcd5841f0f216851cd838e33e17538d
Jawad <jkabir.bscs21seecs@seecs.edu.pk>
2023-05-20 13:49:53 +0500

    calender without func

--- Event Calender/src/App.jsx
+++ Event Calender/src/App.jsx
@@ -1,13 +1,79 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import previous from "../public/images/icons8-back-to-40.png";
+import next from "../public/images/icons8-next-page-40.png";
 import { entireMonth } from "./utils/getMonth";
 
 function App() {
+  const week_days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return (
-    <>
-      {entireMonth(4, 2023).map((item, index) => {
-        return <div key={index}>{item}</div>;
-      })}
-    </>
+    <div className="container">
+      <h1>Event Calender</h1>
+      <div className="row">
+        <div className="col-7">
+          <div className="row justify-content-end ">
+            <div className="col-4">
+              <select id="month" className="form-select">
+                <option defaultValue>Choose...</option>
+                <option value="0">January</option>
+                <option value="1">February</option>
+                <option value="2">March</option>
+                <option value="3">April</option>
+                <option value="4" selected>
+                  May
+                </option>
+                <option value="5">June</option>
+                <option value="6">July</option>
+                <option value="7">August</option>
+                <option value="8">September</option>
+                <option value="9">October</option>
+                <option value="10">November</option>
+                <option value="11">December</option>
+              </select>
+            </div>
+            <div className="col-2">
+              <h3> 2023</h3>
+            </div>
+          </div>
+        </div>
+        <div className="col-5">
+          <button style={{ border: "none" }}>
+            <img src={previous} alt="previous btn" />
+          </button>
+          <button style={{ border: "none" }}>
+            <img src={next} alt="next btn" />
+          </button>
+        </div>
+      </div>
+
+      <div className="row">
+        <table className="table">
+          <thead>
+            <tr>
+              {week_days.map((day) => (
+                <th key={day}>{day}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {entireMonth(4, 2023).map((week, index) => (
+              <tr key={index}>
+                {week.map((date_obj, index) => (
+                  <td
+                    key={index}
+                    className={
+                      date_obj.status === true ? "current" : "not-current"
+                    }
+                  >
+                    {date_obj.date}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
--- Event Calender/src/utils/getMonth.js
+++ Event Calender/src/utils/getMonth.js
@@ -2,15 +2,32 @@
 import dayjs from "dayjs";
 
 export function entireMonth(month, year) {
+  // gets first date of the month
   const firstDate = dayjs().year(year).month(month).startOf("month");
+
+  // gets last date of the month
   const lastDate = dayjs().year(year).month(month).endOf("month");
 
+  // used to store all dates that will be displayed in the 7 by 7 grid
   const allDates = [];
-  const gridDimension = 7 * 7;
+  const thisMonthsDates = [];
+  const nextMonthsDates = [];
+  const previousMonthsDates = [];
+  const gridDimension = 42; // 6 rows by 7 columns
 
+  // getting the days of the current month to fill the grid
   for (let i = firstDate.date(); i <= lastDate.date(); i++) {
+    // get the date of the current day; in this case i
     let date = dayjs().year(year).month(month).date(i).get("date");
-    allDates.push(date);
+    let status = true;
+
+    let dict = {
+      date: date,
+      status: status,
+    };
+
+    // add it in the total dates array
+    thisMonthsDates.push(dict);
   }
 
   // getting the days of the previous month to fill the grid
@@ -19,16 +36,41 @@ export function entireMonth(month, year) {
       .year(year)
       .month(month)
       .date(i - firstDate.day())
+      // 1 of this month is Monday (1), so we subtract 1 to get the last day of the previous month
       .get("date");
-    allDates.unshift(date);
+
+    let status = false;
+    let dict = {
+      date: date,
+      status: status,
+    };
+
+    previousMonthsDates.push(dict);
   }
+
+  // add the previous month dates to the total dates array
+  // as well as the current month dates
+  allDates.push(...previousMonthsDates);
+  allDates.push(...thisMonthsDates);
 
   // getting the days of the next month to fill the grid
   for (let i = 1; i <= gridDimension - allDates.length; i++) {
     let date = dayjs().year(year).month(month).date(i).get("date");
-    allDates.push(date);
+    let status = false;
+
+    let dict = {
+      date: date,
+      status: status,
+    };
+    nextMonthsDates.push(dict);
   }
 
-  console.log(allDates);
-  return allDates;
+  allDates.push(...nextMonthsDates);
+  // converting into a 6 by 7 grid
+  const grid = [];
+
+  while (allDates.length) grid.push(allDates.splice(0, 7));
+
+  console.log(grid);
+  return grid;
 }
